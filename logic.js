e3ae5b86703909907985c5a6e9d699fb4e3c9124
Joshua Zhou <joshuazhou0808@gmail.com>
2024-04-06 16:39:53 -0700

    Finalized X and O front end

--- logic.js
+++ logic.js
@@ -5,8 +5,10 @@
 function initializeBoard(board, clickFunction){
     for (let i = 0; i < 9; i++){
         const cell = document.createElement('img');
+        cell.src = '';
         cell.id = `grid-${i}`;
         cell.index = i;
+        cell.alt = '';
         cell.addEventListener('click', (clickFunction));
         board.appendChild(cell);
         gameboard.push(cell);                   //use gameboard[i] to access a cell at index i
@@ -19,12 +21,12 @@ function initializeBoard(board, clickFunction){
     6 7 8
 */
 
+
 function createPlayer(gameboard, id, img){
     const name = `Player ${id}`;
     const markCell = (index) => {
-        gameboard[index].style.src = `${img}`;
-        gameboard[index].style.class = "clicked";
-        gameboard[index].style.backgroundColor = 'red';
+        gameboard[index].src = `${img}`;
+        gameboard[index].status = "clicked";
         console.log("registered click")
     } 
     return {name, id, img, markCell}
--- main.js
+++ main.js
@@ -3,15 +3,25 @@ const gameboard = new Array();
 
 const board = document.querySelector(".board");
 
-player1 = createPlayer(gameboard, 1, '9')
-player2 = createPlayer(gameboard, 2, 'x')
+player1 = createPlayer(gameboard, 1, 'images/x.png')
+player2 = createPlayer(gameboard, 2, 'images/o.png')
 
 const gameState = (function (player1, player2){
     let curr_player = 1;
-
+    const nextPlayer = document.querySelector('p')
     takeTurn = (event) => {
-        if (curr_player == 1){
-            player1.markCell(event.target.index)
+        if (!(event.target.status == "clicked")){
+            if (curr_player == 1){
+                
+                player1.markCell(event.target.index)
+                curr_player = 2
+                nextPlayer.textContent = `${player2.name}'s Turn`
+            }
+            else if(curr_player == 2){
+                player2.markCell(event.target.index)
+                curr_player = 1
+                nextPlayer.textContent = `${player1.name}'s Turn`
+            }
         }
     }
     return {takeTurn}
